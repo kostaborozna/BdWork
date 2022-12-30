aad2ba61b72607274510eeb20933eb2da040481b
kostaborozna <115942203+kostaborozna@users.noreply.github.com>
2022-12-30 19:46:06 +0300

    python

---
+++ 29/python29/gen_syth_data.py
@@ -0,0 +1,15 @@
+import random
+
+f = open('synth_data.csv', 'w')
+
+for i in range(0, 5):
+	x = random.randint(5, 10)
+	y = random.randint(5, 10)
+	f.write(f"{x},{y},0\n")
+
+for i in range(0, 5):
+	x = random.randint(0, 5)
+	y = random.randint(0, 5)
+	f.write(f"{x},{y},0\n")
+
+f.close()
---
+++ 29/python29/train.py
@@ -0,0 +1,13 @@
+from sklearn.cluster import KMeans
+
+import numpy as np
+
+data = np.genfromtxt('synth_data.csv', delimiter=',')
+
+xy = data[:, [0,1]]
+z = data[:, 2].astype('uint32')
+
+km = KMeans(n_clusters=2, n_init=5)
+km.fit(xy, z)
+
+print(km.cluster_centers_)
